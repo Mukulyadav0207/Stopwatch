a68b1dfb921289b73d28be232ae8dee6771307ef
Mukul Yadav <mukulyadav0207@gmail.com>
2023-10-14 20:18:32 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import Stopwatch from "./components/Stopwatch";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <Stopwatch />
+      </div>
+    </>
+  );
+}
+
+export default App;
